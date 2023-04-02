d2d012ecce84fdd8d7e1d91f69cded96115116d2
Xiaotian Wu <yetist@gmail.com>
2023-04-02 09:44:54 +0800

    import repos from Archlinux
    date: 20230402
    svn-packages:  d76c6d8f515679417efe599764f43825bdf08d4f
    svn-community: 64f7cf75f55a711145748e4976b1968db1bb43ec

--- community/sweethome3d/sweethome3d.sh
+++ community/sweethome3d/sweethome3d.sh
@@ -24,36 +24,17 @@ fi
 # Find Java version
 JAVA_VERSION="$(${JAVA_EXEC} -version 2>&1 | grep ' version ' | head -1 | cut -d' ' -f 3 | tr -d '"')"
 
-# Skip Java versions higher than 16 as Sweethome3D actually is not yet compatible
-# Fallback to any previous version available
-# https://bugs.archlinux.org/task/72623
-if [ $(vercmp "${JAVA_VERSION}" "17") -gt 0 ]
-then
-  echo "Warning: Sweethome 3D actually is not compatible with Java version > 16"
-  _PREVIOUS_JAVA_VERSION="$(archlinux-java status | tail -n +2 | sort | cut -d ' ' -f 3 | sort -nr -k 2 -t '-' | grep -vE '17-|18-' -m 1)"
-  if [ -z "${_PREVIOUS_JAVA_VERSION}" ]
-  then
-    echo "No others Java version are available, please install a Java version < 17"
-    exit 1
-  else
-    echo "Falling back to ${_PREVIOUS_JAVA_VERSION}"
-    export JAVA_HOME="/usr/lib/jvm/${_PREVIOUS_JAVA_VERSION}"
-    export JAVA_EXEC="${JAVA_HOME}/bin/java"
-    export JAVA_VERSION="$(${JAVA_EXEC} -version 2>&1 | grep ' version ' | head -1 | cut -d' ' -f 3 | tr -d '"')"
-  fi
-fi
-
 # Fix for Java 16 compatibility
 # https://bugs.archlinux.org/task/71255
 # https://sourceforge.net/p/sweethome3d/bugs/1021/
-if [ $(vercmp "${JAVA_VERSION}" "11") -gt 0 ]
+if [ $(vercmp "${JAVA_VERSION}" "16") -gt 0 ]
 then
   # Add illegal-access=permit argument
-  JAVA_OPTS="${JAVA_OPTS} --illegal-access=permit"
+  JAVA_OPTS="${JAVA_OPTS} --add-opens=java.desktop/sun.awt=ALL-UNNAMED --add-opens=java.desktop/com.apple.eio=ALL-UNNAMED --add-opens=java.desktop/com.apple.eawt=ALL-UNNAMED"
 fi
 
 # Build classpath
-APP_CLASSPATH="SweetHome3D.jar:Furniture.jar:Textures.jar:Examples.jar:Help.jar:iText-2.1.7.jar:freehep-vectorgraphics-svg-2.1.1b.jar:sunflow-0.07.3i.jar:jmf.jar:batik-svgpathparser-1.7.jar:jeksparser-calculator.jar:jnlp.jar"
+APP_CLASSPATH="SweetHome3D.jar:Furniture.jar:Textures.jar:Examples.jar:Help.jar:iText-2.1.7.jar:freehep-vectorgraphics-svg-2.1.1c.jar:sunflow-0.07.3i.jar:jmf.jar:batik-svgpathparser-1.7.jar:jeksparser-calculator.jar:jnlp.jar"
 if [ "${SWEETHOME3D_JAVA3D}" = "1.6" ]
 then
   JAVA_OPTS="${JAVA_OPTS} -Djava.library.path=/usr/lib/sweethome3d/java3d-1.6 -Djogamp.gluegen.UseTempJarCache=false"
